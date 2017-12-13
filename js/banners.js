10fb0c6f6206d7f501ec01f3ff9fc1dbcc6fd9f7
TheEscape94 <maperlic@gmail.com>
2017-12-13 05:36:00 +0100

    new banners

--- js/banners.js
+++ js/banners.js
@@ -7,5 +7,11 @@ $(document).ready(function () {
         appendImage('q1313', 'werige.jpg', 'Klub Werige','http://google.rs/', 8, 4);
         appendImage('q132', 'giardino.jpg', 'Klub Giardino','http://google.rs/', 3, 3);
         appendImage('q1539', 'cactus.jpg', 'Klub Cactus','http://google.rs/', 2, 2);
-        appendImage('q019', 'djava.png', 'Klub Cactus','http://google.rs/', 3, 3);
+        appendImage('q019', 'djava.png', 'Klub Cactus','http://google.rs/', 4, 3);
+        appendImage('q190', 'cubalibre.jpg', 'Cuba Libre, Laze Teleckog','http://google.rs/', 5, 5);
+        appendImage('q50', '212.jpg', 'Atelje 212, Laze Teleckog','http://google.rs/', 9, 6);
+        appendImage('q723', 'thepub.png', 'The Pub','http://google.rs/', 3, 3);
+        appendImage('q1943', 'kafemat.png', 'The Pub','http://google.rs/', 7, 6);
+        appendImage('q1445', 'tdcafe.jpg', 'TD music','http://google.rs/', 5, 5);
+        appendImage('q047', 'amico.jpg', 'TD music','http://google.rs/', 3, 3);
 });
--- js/comments.js
+++ js/comments.js
@@ -6,5 +6,7 @@ $(document).ready(function () {
     appendComment('https://www.google.rs', 'werige.jpg', 1, '14 osoba', 'Ovde je bio sam krem Novog Sada. Prelepe devojke, dobra muzika, usluga i pice na visini zadatka!');
     appendComment('https://www.google.rs', 'paradiso.jpg', 1, '104 osobe', 'Za proslu najludju noc, samo je na trgu bilo vise ljudi nego ovde. Vrh!!!');
     appendComment('https://www.google.rs', 'museum.jpg', 1, '24 osobe', 'Koliko smo saznali ovde jednevna zurka bila ok, ali uvece je gorelo!!!');
-    
+    appendComment('https://www.google.rs', '212.jpg', 0, '27 osoba', 'Koliko smo saznali ovde jednevna zurka bila ok, ali uvece je gorelo!!!');
+    appendComment('https://www.google.rs', 'cubalibre.jpg', 0, '7 osoba', 'Koliko smo saznali ovde jednevna zurka bila ok, ali uvece je gorelo!!!');
+    appendComment('https://www.google.rs', 'kafemat.png', 1, '77 osoba', 'Koliko smo saznali ovde jednevna zurka bila ok, ali uvece je gorelo!!!');
 });
